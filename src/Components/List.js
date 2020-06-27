9dd4500ed5af1ce37d959152ed0f391f9a9c2146
hishambokhari <hish.b92@gmail.com>
2020-06-28 00:38:43 +0500

    Add: clear button

--- src/Components/List.js
+++ src/Components/List.js
@@ -4,10 +4,11 @@ import Item from './Item';
 export default class List extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1> 
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
         <Item />
-      </div>
-    )
+        <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">clear list</button>
+      </ul>
+    );
   }
 }
